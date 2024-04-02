a0de3527c9ba4bac20e9fd09fb4a1275a5d3841e
anasu <1774695537@qq.com>
2024-04-02 11:10:18 +0800

    updated on 4/2

--- Labs/lab12/lab12.scm
+++ Labs/lab12/lab12.scm
@@ -1,6 +1,7 @@
 
 (define-macro (def func args body)
-    'YOUR-CODE-HERE)
+    `(define ,(cons func args) ,body)
+)
 
 
 (define (map-stream f s)
@@ -9,17 +10,27 @@
     	(cons-stream (f (car s)) (map-stream f (cdr-stream s)))))
 
 (define all-three-multiples
-  'YOUR-CODE-HERE
+  (map-stream (lambda (x) (+ x 3)) (cons-stream 0 all-three-multiples))
 )
 
 
 (define (compose-all funcs)
   'YOUR-CODE-HERE
+  (if (null? funcs)
+    (lambda (x) x)
+    (lambda (x) ((compose-all (cdr funcs)) ((car funcs) x)))
+  )
 )
 
 
 (define (partial-sums stream)
   'YOUR-CODE-HERE
+  (define (helper ans stream)
+    (if (null? stream)
+      nil
+      (cons-stream (+ ans (car stream)) (helper (+ ans (car stream)) (cdr-stream stream)))
+    )
+  )
   (helper 0 stream)
 )
 
